6895166241dbd2d8ff65b574ad69976fe8e5678a
wgch <1153540212@qq.com>
2020-08-24 17:14:59 +0800

    2020年8月23日17:08:00

---
+++ eureka-server/src/test/java/com/wdy/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdy.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
